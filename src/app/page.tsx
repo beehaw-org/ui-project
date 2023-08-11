4a375f45c26aa21f92db249e141ad88652e0fa31
sunaurus <sander@saarend.com>
2023-08-11 14:45:49 +0300

    Make content a bit wider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ const Page = async () => {
   const { posts } = await apiClient.getPosts({});
 
   return (
-    <div className='lg:flex max-w-screen-lg mx-auto space-x-4'>
-      <article className='w-2/3 space-y-2'>
+    <div className='lg:flex max-w-screen-xl mx-auto space-x-4'>
+      <article className='w-3/4 space-y-2'>
         {posts.map((postView: PostView) => (
           <PostCard key={postView.post.id} postView={postView} />
         ))}
       </article>
-      <Sidebar className='w-1/3' />
+      <Sidebar className='w-1/4' />
     </div>
   );
 };
