7bb33e2959b6da6e851fede45b9a36d7293793f7
Lionir <git@thelion.website>
2023-08-26 07:55:05 -0400

    Add a preliminary version of the post page to build upon  (#46)
    This entirely reuses PostCard with no modification. It probably should
    but it could be used as a base to build upon and tweak.

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,5 +1,20 @@
-const Page = ({ params }: { params: { id: string } }) => {
-  return <h1>this is a post with id: {params.id}</h1>;
+import Sidebar from '@components/desktop-exclusive/Sidebar/MainSidebar';
+import { PostCard } from '@components/common/PostCard';
+import { apiClient } from '@app/apiClient';
+
+const PostPage = async ({ params }: { params: { id: Number } }) => {
+  const { post_view } = await apiClient.getPost({
+    id: Number(params.id),
+  });
+
+  return (
+        <div className='lg:flex max-w-screen-lg mx-auto space-x-4'>
+          <article className='w-2/3 space-y-2'>
+            <PostCard key={post_view.post.id} postView={post_view} />
+          </article>
+          <Sidebar className='w-1/3' />
+      	</div>
+    );
 };
 
-export default Page;
+export default PostPage;
